4fe743eb49f60843f9ceb2f54647df696424df4b
Dariusz Kwiatkowski <ktoztam@gmail.com>
2018-11-01 22:20:39 +0100

    url encodes now request - query can be invoked by url

--- src/app/collector.service.ts
+++ src/app/collector.service.ts
@@ -11,9 +11,10 @@ export class CollectorService {
 
   public observedRecords: Subject<any> = new BehaviorSubject<any>({});
   public observedMetadata: Subject<any> = new Subject();
-  public defaultPageSize = 100;
+  public loading: Subject<boolean> = new BehaviorSubject<any>(true);
+
   private lastSearchType: SearchType = SearchType.TEXT;
-  private lastFilter: string;
+  private lastFilter: string = '';
 
   private recordCount: number;
   private totalRecordCount: number;
@@ -23,35 +24,38 @@ export class CollectorService {
   private backOption: boolean = false;
   private forwardOption: boolean = false;
 
-
-  constructor(private httpClient: HttpClient) {}
-
-  getFilteredRecords(filter: string, page: number = 0): any {
-
-    const width = window.innerWidth
-      || document.documentElement.clientWidth
-      || document.body.clientWidth;
-
-    const pageSize = (width <= 600) ? 10 : 50;
-
-    const options =  {
-        params: new HttpParams()
-          .set('filter', filter)
-          .set('pageSize', pageSize + '')
-          .set('page', page + '')
-    };
-
-    this.httpClient.get('/', options).subscribe((data:any) => {
-      this.observedRecords.next(data);
-      this.lastSearchType = SearchType.TEXT;
-      this.lastFilter = filter;
-      this.parseRecords(data);
-      this.updateMetadata();
-    });
-
+  constructor(private httpClient: HttpClient) {
   }
 
-  parseRecords(data:any) {
+  getFilteredRecords(filter: string, page: number = 0, type: SearchType = SearchType.TEXT, pageSize?): any {
+
+    pageSize = filter !== this.lastFilter && !pageSize ? this.deviceDependsPageSize() : pageSize || this.pageSize;
+
+    let queryParams = new HttpParams()
+      .set('pageSize', pageSize + '')
+      .set('page', page + '');
+
+    queryParams = type === SearchType.TEXT ? queryParams.set('filter', filter) : queryParams.set('source', filter);
+
+    const options = {
+      params: queryParams
+    };
+
+    const url = type === SearchType.TEXT ? '/' : '/source';
+
+    this.loading.next(true);
+
+    this.httpClient.get(url, options).subscribe((data: any) => {
+      this.observedRecords.next(data);
+      this.lastSearchType = type;
+      this.lastFilter = filter ? filter : this.lastFilter;
+      this.parseRecords(data);
+      this.updateMetadata();
+      this.loading.next(false);
+    });
+  }
+
+  parseRecords(data: any) {
     this.recordCount = (data.recordsOnPage > 0) ? data.pageNumber * data.defaultPageSize + 1 : 0;
     this.totalRecordCount = data.recordCountTotal;
     this.pageSize = data.defaultPageSize;
@@ -59,6 +63,14 @@ export class CollectorService {
     this.pageNumber = data.pageNumber;
     this.backOption = (this.recordCount > 1);
     this.forwardOption = (this.upToIndex < this.totalRecordCount);
+  }
+
+  deviceDependsPageSize(): number {
+    const width = window.innerWidth
+      || document.documentElement.clientWidth
+      || document.body.clientWidth;
+
+    return (width <= 600) ? 10 : 50;
   }
 
   updateMetadata() {
@@ -72,45 +84,18 @@ export class CollectorService {
       forwardOption: this.forwardOption,
       filter: this.lastFilter,
       lastSearchType: this.lastSearchType
-    })
+    });
   }
 
   nextPage() {
-    if(this.forwardOption) {
-      if(this.lastSearchType === SearchType.SOURCE) this.getRecordsFromSource(this.lastFilter, this.pageNumber + 1);
-      else if(this.lastSearchType === SearchType.TEXT) this.getFilteredRecords(this.lastFilter, this.pageNumber + 1)
+    if (this.forwardOption) {
+      this.getFilteredRecords(this.lastFilter, this.pageNumber + 1, this.lastSearchType);
     }
   }
 
   previousPage() {
-    if(this.backOption) {
-      if(this.lastSearchType === SearchType.SOURCE) this.getRecordsFromSource(this.lastFilter, this.pageNumber - 1);
-      else if(this.lastSearchType === SearchType.TEXT) this.getFilteredRecords(this.lastFilter, this.pageNumber - 1)
+    if (this.backOption) {
+      this.getFilteredRecords(this.lastFilter, this.pageNumber - 1, this.lastSearchType);
     }
-  }
-
-  getRecordsFromSource(source: string, page: number = 0) {
-
-    const width = window.innerWidth
-      || document.documentElement.clientWidth
-      || document.body.clientWidth;
-
-    const pageSize = (width <= 600) ? 10 : 50;
-
-    const options =  {
-      params: new HttpParams()
-        .set('source', source)
-        .set('pageSize', pageSize + '')
-        .set('page', page + '')
-    };
-
-    this.httpClient.get('/source', options).subscribe((data)=>{
-      this.observedRecords.next(data);
-      this.lastSearchType = SearchType.SOURCE;
-      this.lastFilter = source;
-      this.parseRecords(data);
-      this.updateMetadata();
-    });
-
   }
 }
